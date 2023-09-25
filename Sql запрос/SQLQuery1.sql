3d8c9df2b002aa3f389d7dfd7dd7e32f4158085c
Pavel Maslov <maslovpavel333@gmail.com>
2023-09-25 04:17:36 +0300

    Удаление работу с Sql

--- Sql запрос/SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-Select
-    P.Name as Product,
-    C.Name as Category
-From
-    Products as P
-Left Join
-    ProductCategory as PC on P.Id = PC.ProductId
-Left Join
-    Categories as C on PC.CategoryId = C.Id;
